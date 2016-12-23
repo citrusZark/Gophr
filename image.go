1cfa82d30b661972280afc63ceb42f14842c1862
pangeran.thoha <pangeran@outlook.com>
2016-12-23 15:37:56 +0700

    - Initial commit 2

--- image.go
+++ image.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"image"
 	"io"
 	"mime"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
 	"time"
+
+	"github.com/disintegration/imaging"
 )
+
+func init() {
+	// Ensure our goroutines run across all cores
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
 
 const (
 	imageIDLength = 10
+	widthTumbnail = 400
+	widthPreview  = 800
 )
 
 // A map of accepted mime types and their file extension
@@ -57,6 +68,13 @@ func (image *Image) CreatedFromFile(file multipart.File, headers *multipart.File
 		return err
 	}
 	image.Size = size
+
+	// Create the various resizes of the images
+	err = image.CreatedResizedImages()
+	if err != nil {
+		return err
+	}
+
 	// Save the image to the database
 	db := NewDBImageStore()
 	defer db.Close()
@@ -108,6 +126,12 @@ func (image *Image) CreatedFromURL(imageUrl string) error {
 	// The returned value from io.Copy is the number of bytes copied
 	image.Size = size
 
+	// Create the various resizes of the images
+	err = image.CreatedResizedImages()
+	if err != nil {
+		return err
+	}
+
 	// Save our image to the store
 	db := NewDBImageStore()
 	defer db.Close()
@@ -122,4 +146,57 @@ func (image *Image) StaticRoute() string {
 // This method automatically called by html template
 func (image *Image) ShowRoute() string {
 	return "/image/" + image.ID
+}
+
+func (image *Image) CreatedResizedImages() error {
+	// generate an image from file
+	srcImage, err := imaging.Open("./data/images/" + image.Location)
+	if err != nil {
+		return err
+	}
+
+	// Create a channel to receive errors on
+	errorChan := make(chan error)
+
+	// Process each size
+	go image.resizePreview(errorChan, srcImage)
+	go image.resizeThumbnail(errorChan, srcImage)
+
+	// Wait for images to finish resizing
+	/*We could have returned early if we discovered an error; however, it would mean
+	that if we received an error on the first iteration of the loop and returned, the
+	second iteration would never occur and the second goroutine never complete.
+	This would cause the goroutine to remain in memory.*/
+	var e error
+	for i := 0; i < 2; i++ {
+		e = <-errorChan
+		if err == nil {
+			err = e
+		}
+	}
+	return err
+}
+
+func (image *Image) resizeThumbnail(errorChan chan error, srcImage image.Image) {
+	dstImage := imaging.Thumbnail(srcImage, widthTumbnail, widthTumbnail, imaging.Lanczos)
+
+	destination := "./data/images/thumbnail/" + image.Location
+	errorChan <- imaging.Save(dstImage, destination)
+}
+
+func (image *Image) resizePreview(errorChan chan error, srcImage image.Image) {
+	size := srcImage.Bounds().Size()
+	ratio := float64(size.Y) / float64(size.X)
+	targetHeight := int(float64(widthPreview) * ratio)
+
+	dstImage := imaging.Resize(srcImage, widthPreview, targetHeight, imaging.Lanczos)
+	destination := "./data/images/preview/" + image.Location
+	errorChan <- imaging.Save(dstImage, destination)
+}
+
+func (image *Image) StaticThumbnailRoute() string {
+	return "/im/thumbnail/" + image.Location
+}
+func (image *Image) StaticPreviewRoute() string {
+	return "/im/preview/" + image.Location
 }
